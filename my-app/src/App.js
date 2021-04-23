f1638e003f3caf2dd09dda91a81de4cc391dd38c
William Dye <williamdye@williams-mbp.myfiosgateway.com>
2021-04-23 08:01:24 -0400

    Nav bar has been turned into a component, links now direct to the appropriate components

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,32 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
-import { Link, Route, Switch } from 'react-router-dom'
+import NavBar from './Components/NavBar';
 
 function App() {
   return (
     <div>
-      <nav>
-        <div className='nameHeader'>
-          <h1>Empamama's</h1>
-        </div>
-        <div className='linksHeader'>
-          <Link to="/">Home</Link>
-          <Link to="/About">About</Link>
-          <Link to="/Contact">Contact</Link>
-        </div>
-      </nav>
-
-      <Switch>
-        <Route path={'/Home'}></Route>
-        <Route path={'/About'}></Route>
-        <Route path={'/Contact'}></Route>
-      </Switch>
+      <NavBar />
     </div>
-
-    
-    
-    
-
   );
 }
 
---
+++ my-app/src/Components/NavBar.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { Link, Route, Switch } from 'react-router-dom';
+import Home from '../Home';
+import About from '../About';
+import Contact from '../Contact'
+
+export default function NavBar() {
+    return (
+        <div>
+          <nav>
+            <div className='nameHeader'>
+              <h1>Empamama's</h1>
+            </div>
+            <div className='linksHeader'>
+              <Link to="/">Home</Link>
+              <Link to="/About">About</Link>
+              <Link to="/Contact">Contact</Link>
+            </div>
+          </nav>
+    
+          <Switch>
+            <Route path={'/Home'}>
+                <Home />
+            </Route>
+            <Route path={'/About'}>
+                <About />
+            </Route>
+            <Route path={'/Contact'}>
+                <Contact />
+            </Route>
+          </Switch>
+        </div>
+    
+        
+        
+        
+    
+      );
+}
