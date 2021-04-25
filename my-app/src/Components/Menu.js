61c9b03ecfaf3f64f2388fc3970b1afc33472ebe
William Dye <williamdye@williams-mbp.myfiosgateway.com>
2021-04-25 04:07:13 -0400

    Laid groundwork for initial menu

--- my-app/src/Components/Menu.js
+++ my-app/src/Components/Menu.js
@@ -3,6 +3,27 @@ import React from 'react';
 export default function Menu () {
 
     return (
-        <h2>Menu: Don't order the Steeg</h2>
+        <div>
+            <section class="menuHeader">
+                <h2>Menu</h2>
+                <h4>Choose from any of our tapas-style delicacies</h4>
+            </section>
+            <section class="menuItems">
+                <div class="menuItem1">
+                    <h3>Empanada</h3>
+                    <p>A baked pastry stuffed with meat</p>
+                </div>
+                <div class="menuItem2">
+                    <h3>Lumpia</h3>
+                    <p>A Filipino spring roll</p>
+                </div>
+                <div class="menuItem3">
+                    <h3>Longganisa</h3>
+                    <p>A Filipino sausage</p>
+                </div>
+            </section>
+            
+            
+        </div>
     )
 }
