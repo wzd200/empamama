b555b931215db4f2247fa1082c1ddff1f062f2fc
William Dye <williamdye@williams-mbp.myfiosgateway.com>
2021-04-25 03:24:40 -0400

    Slight correction to the about text

--- my-app/src/Components/About.js
+++ my-app/src/Components/About.js
@@ -8,8 +8,8 @@ export default function About () {
                 <div class = "aboutTextContainer">
                     <div class = "aboutText">
                         <h2>About Empamama's</h2>
-                        <p>Empamama’s is your ticket to experiencing an authentic Filipino experience in Southern Delaware!</p>
-                        <p>Empamama's started as a vision from a proud Filipina to spread the warmth and comfort of traditional Filipino cooking to her neighbors - and we want to share that enduring vision with you!</p>
+                        <p>Empamama’s is your ticket to experiencing an authentic Filipino meal in Southern Delaware!</p>
+                        <p>Empamama's started as a vision from a proud Filipina to spread the warmth and comfort of traditional Filipino cooking to her neighbors - and we want to share that enduring vision with you.</p>
                         <p>Feel free to contact us to schedule a Filipino-style dinner or make an order for our homemade frozen Filipino delicacies.</p>
                     </div>
                 </div>
