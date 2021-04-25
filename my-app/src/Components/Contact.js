0d1614f9659216914805de690339ae90e860e05f
William Dye <williamdye@williams-mbp.myfiosgateway.com>
2021-04-25 15:06:18 -0400

    Completed contact page

--- my-app/src/Components/Contact.js
+++ my-app/src/Components/Contact.js
@@ -3,6 +3,15 @@ import React from 'react';
 export default function Contact () {
 
     return (
-        <h2>Contact: Do not contact Steeg at steeg@steeg.com plz and thnk.</h2>
+        <div>
+            <div class="contactBackground">
+                <div class="contactText">
+                    <h2>Interested in contacting us for a Filipino-style dinner or an order of our frozen tapas to cook at home?</h2>
+                    <h3>Please contact us at the following:</h3>
+                    <p>Email: empamama@empamama.com</p>
+                    <p>Phone: (555) 555 - 5555</p>
+                </div>
+            </div>
+        </div>
     )
 }
--- my-app/src/Components/Menu.js
+++ my-app/src/Components/Menu.js
@@ -4,24 +4,26 @@ export default function Menu () {
 
     return (
         <div>
-            <section class="menuHeader">
-                <h2>Menu</h2>
-                <h4>Choose from any of our tapas-style delicacies</h4>
-            </section>
-            <section class="menuItems">
-                <div class="menuItem1">
-                    <h3>Empanada</h3>
-                    <p>A baked pastry stuffed with meat</p>
-                </div>
-                <div class="menuItem2">
-                    <h3>Lumpia</h3>
-                    <p>A Filipino spring roll</p>
-                </div>
-                <div class="menuItem3">
-                    <h3>Longganisa</h3>
-                    <p>A Filipino sausage</p>
-                </div>
-            </section>
+            <div class="menuBackground">
+                <section class="menuHeader">
+                    <h2>Menu</h2>
+                    <h4>Choose from any of our tapas-style delicacies:</h4>
+                </section>
+                <section class="menuItems">
+                    <div class="menuItem1">
+                        <h3>Empanada</h3>
+                        <p>A baked pastry stuffed with meat - 4</p>
+                    </div>
+                    <div class="menuItem2">
+                        <h3>Lumpia</h3>
+                        <p>A Filipino spring roll - 3</p>
+                    </div>
+                    <div class="menuItem3">
+                        <h3>Longganisa</h3>
+                        <p>A Filipino sweet sausage - 3</p>
+                    </div>
+                </section>
+            </div>
             
             
         </div>
